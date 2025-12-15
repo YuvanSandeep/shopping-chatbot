95c998d9ff2fa994eaacdfbf8bdbcf307208cb3a
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2025-12-15 18:46:21 +0000

    adding comments to assignment

--- assignment.py
+++ assignment.py
@@ -1,7 +1,14 @@
 name = input ("What is your name?")
+# asking personal info
 age = int(input("What is your age?"))
+# asking personal info
 hobbies = input ("What hobbies do you have?")
+# asking personal info
 fav_col = input ("What is your favourite colour?")
+# asking personal info
 fav_song = input ("What is your favourite song?")
+# asking personal info
 fav_movie = input ("What is your favourite movie?")
+# asking personal info
 print (F"Your name is {name}, aged {age} . Your favourite hobby/hobbies is/are {hobbies} and your favourite colour is {fav_col}.Also, your favourite song is {fav_song} and your favourite movie is {fav_movie}.")
+# printing all the personal info
